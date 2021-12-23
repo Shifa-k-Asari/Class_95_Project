5dd8d9d9e57ec426fcab49be465e9d9eac5393fa
Shifa-k-Asari <90330560+Shifa-k-Asari@users.noreply.github.com>
2021-12-23 15:15:53 +0530

    Delete Kwitter.js

--- Kwitter.js
+++
@@ -1,8 +0,0 @@
-function addUser()
-{
-    Name_of_user = document.getElementById("user_name").value;
-
-    localStorage.setItem("Name of user", Name_of_user);
-
-    window.location = "Kwitter_room.html";
-}
